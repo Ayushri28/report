06f1b2a707ee1e4e1fb63967af0b1203ffbb139f
Ayushri28 <Ayushi.Shrivastava2@mindtree.com>
2018-09-15 23:30:46 +0530

    commit

---
+++ ReportNg1/src/main/java/ReportNg1/ReportNg1/App.java
@@ -0,0 +1,13 @@
+package ReportNg1.ReportNg1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ReportNg1/src/main/java/reportNgEx1/Example.java
@@ -0,0 +1,20 @@
+package reportNgEx1;
+
+import org.testng.annotations.Test;
+
+public class Example {
+
+	
+	@Test
+	public void f()
+	{
+		System.out.println("first test case");
+	}
+	
+
+	@Test
+	public void g()
+	{
+		System.out.println("second test case");
+	}
+}
